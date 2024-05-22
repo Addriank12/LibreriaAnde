e472221f970104d880eae18471a7ee6ca5a22c43
Addriank12 <addriank12@gmail.com>
2024-05-22 00:31:58 -0500

    About y Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './Pages/home/home.component';
 import { ShopComponent } from './Pages/shop/shop.component';
 import { LocationsComponent } from './Pages/locations/locations.component';
 import { AboutItComponent } from './Pages/about-it/about-it.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'shop', component: ShopComponent },
     { path: 'locations', component: LocationsComponent },
-    { path: 'aboutIt', component: AboutItComponent }
+    { path: 'aboutIt', component: AboutItComponent },
+    { path: 'login', component: LoginComponent }
 ];
 
 
