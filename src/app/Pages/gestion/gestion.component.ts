cd5edfd6f79a02e47957685bf57788e4679e2f25
Marco Cajamarca <marcoxc01@gmail.com>
2024-05-28 12:48:56 -0500

    Added new styles on the Home Pagee

--- src/app/Pages/gestion/gestion.component.ts
+++ src/app/Pages/gestion/gestion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/app';
+
 
 @Component({
   selector: 'app-gestion',
@@ -8,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrl: './gestion.component.css'
 })
 export class GestionComponent {
+  nombre: string = '';
+  autor: string = '';
+  genero: string = '';
+  fecha: string = '';
+  imagen: string = '';
 
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "TU_API_KEY",
+      authDomain: "TU_AUTH_DOMAIN",
+      projectId: "TU_PROJECT_ID",
+      storageBucket: "TU_STORAGE_BUCKET",
+      messagingSenderId: "TU_MESSAGING_SENDER_ID",
+      appId: "TU_APP_ID"
+    };
+
+    //firebase.initializeApp(firebaseConfig);
+  }
+
+  agregarLibro() {
+    // Guardar el nuevo libro en Firebase
+    //firebase.database().ref('libros').push
+    ({
+      nombre: this.nombre,
+      autor: this.autor,
+      genero: this.genero,
+      fecha: this.fecha,
+      imagen: this.imagen
+    });
+
+    // Limpiar el formulario después de agregar el libro
+    this.nombre = '';
+    this.autor = '';
+    this.genero = '';
+    this.fecha = '';
+    this.imagen = '';
+  }
 }
