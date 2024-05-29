4664a5190a289b6ccf8d574eab24d1ae316f6691
Marco Cajamarca <marcoxc01@gmail.com>
2024-05-29 10:58:18 -0500

    Update en la pagina home, locations, and profile components, tambien improving la pagina de login

--- src/app/Pages/profile/profile.component.ts
+++ src/app/Pages/profile/profile.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Services/auth.service';
 import { onAuthStateChanged } from '@firebase/auth';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit{
   userName: string | null = null;
   userEmail: string | null = null;
+  isEditing = false;
   
   constructor(private authService: AuthService) { }
 
@@ -23,5 +25,10 @@ export class ProfileComponent implements OnInit{
         this.userEmail = user.email;
       }
     });
+  }
+
+  saveChanges() {
+    // code to save changes goes here
+    this.isEditing = false;
   }
 }
