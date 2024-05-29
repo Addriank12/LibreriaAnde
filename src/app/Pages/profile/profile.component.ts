9ea3c54e0f131b5aa129a258f86c3d5835bf5ce7
Marco Cajamarca <marcoxc01@gmail.com>
2024-05-28 19:20:22 -0500

    Agregado la Profile Page, where the user can ver sus datos

---
+++ src/app/Pages/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserInfoService } from '../../Services/user-info.service';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { ShopComponent } from './Pages/shop/shop.component';
 import { LocationsComponent } from './Pages/locations/locations.component';
+import { ProfileComponent } from './Pages/profile/profile.component';
 import { AboutItComponent } from './Pages/about-it/about-it.component';
 import { AuthenticationComponent } from './Pages/authentication/authentication.component';
+
 
 
 export const routes: Routes = [
@@ -12,7 +14,8 @@ export const routes: Routes = [
     { path: 'shop', component: ShopComponent },
     { path: 'locations', component: LocationsComponent },
     { path: 'aboutIt', component: AboutItComponent },
-    { path: 'login', component: AuthenticationComponent }
+    { path: 'login', component: AuthenticationComponent },
+    { path: 'profile', component: ProfileComponent }
 ];
 
 
