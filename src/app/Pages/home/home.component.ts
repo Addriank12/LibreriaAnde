1b6e38b1fd5fc58bc7d35fe5461a99b7f8c3a2c6
Marco Cajamarca <marcoxc01@gmail.com>
2024-05-30 00:06:54 -0500

    Mostrando Existencias

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LibroService } from '../../Services/libro.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  libros: any[] = [];
 
+  constructor(private libroService: LibroService) {}
+
+  async ngOnInit() {
+    this.libros = await this.libroService.getLibros();
+    console.log(this.libros);
+  }
   
-
 }
--- src/app/Pages/shop/shop.component.ts
+++ src/app/Pages/shop/shop.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LibroService } from '../../Services/libro.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  libros: any[] = [];
+
+  constructor(private libroService: LibroService) {}
+
+  async ngOnInit() {
+    this.libros = await this.libroService.getLibros();
+    console.log(this.libros);
+  }
 
 }
